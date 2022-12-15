238996b31827c5a005b937895a7ea2aa2ebc3f67
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2022-12-15 12:54:02 -0300

    Apply several changes

--- EmpresaELocaisDeEstoque.sql
+++ EmpresaELocaisDeEstoque.sql
@@ -1,21 +1,29 @@
+/**************************************************************************************
+
+  DESCRIÇÃO
+  -------------------------------------------------------------------------------------
+  Esse Script tem como objetivo facilitar a visualização por Empresas e seus locais de
+  estoque.
+
+**************************************************************************************/
+
 SELECT
-  a.IdEmpresa,
-  a.Empresa,
-  b.IdLocal,
-  b.DescriçãoLocal,
-  a.IdLocalEstoquePadrão,
-  a.Estado,
-  c.Cidade,
-  a.País,
-  a.Endereço,
-  a.Bairro,
-  a.str_complemento
+  [a].[IdEmpresa],
+  [a].[Empresa],
+  [b].[IdLocal],
+  [b].[DescriçãoLocal],
+  [a].[IdLocalEstoquePadrão],
+  [a].[Estado],
+  [c].[Cidade],
+  [a].[País],
+  [a].[Endereço],
+  [a].[Bairro],
+  [a].[str_complemento]
 FROM
-  Tab_Empresa AS a
-    INNER JOIN
-  Tab_LocaisEstoque AS b
-      ON a.IdEmpresa = b.IDEmpresa
-    INNER JOIN
-  Tab_Cidades AS c
-      ON a.IdCidade = c.IdCidade
-ORDER BY a.IdEmpresa, b.IdLocal
+  [Tab_Empresa] AS [a]
+  INNER JOIN
+  [Tab_LocaisEstoque] AS [b] ON [a].IdEmpresa = [b].IDEmpresa
+  INNER JOIN
+  [Tab_Cidades] AS [c] ON [a].[IdCidade] = [c].[IdCidade]
+ORDER BY
+  [a].[IdEmpresa], [b].[IdLocal]
--- FollowWeb/Ajustar horário e pendências.sql
+++ FollowWeb/Ajustar horário e pendências.sql
@@ -1,61 +1,58 @@
 DECLARE
-    @IdUsuario AS INT   = 1226 -- Meu usuário
-  , @IdPendencia AS INT = ''
-  , @Data AS DATETIME   = ''   -- Formato [yyyy-MM-dd]
+    @IdUsuario AS INT   = 1226 -- (Meu usuário)
+  , @IdPendencia AS INT = '<ID da Pendência, INT, >'
+  , @Data AS DATETIME   = ''   -- Formato da Data [yyyy-MM-dd]
 
 SELECT
-    [pendenciahora].IdPendenciaHoras
-  , [usuario].IdUsuario
-  , [usuario].PrimeiroNome
-  , [usuario].Email
-  , [pendenciahora].IdPendencia
-  , [pendencia].Assunto
-  , [tipohora].DescricaoTipoHora
-  , [pendenciahora].Data
-  , [pendenciahora].HoraInicio
-  , [pendenciahora].HoraFim
+    [pendenciahora].[IdPendenciaHoras] AS [IdPendenciaHoras]
+  , [node].[Descricao]                 AS [Pendência na Pasta]
+  , [usuario].[PrimeiroNome]           AS [PrimeiroNome]
+  , [usuario].[Email]                  AS [Email]
+  , [pendenciahora].[IdPendencia]      AS [IdPendencia]
+  , [pendencia].[Assunto]              AS [Assunto]
+  , [tipohora].[DescricaoTipoHora]     AS [DescricaoTipoHora]
+  , [pendenciahora].[Data]             AS [Data]
+  , [pendenciahora].[HoraInicio]       AS [HoraInicio]
+  , [pendenciahora].[HoraFim]          AS [HoraFim]
 FROM
   [Pendencias_Horas] AS [pendenciahora] WITH(NOLOCK)
-    INNER JOIN
-  [Usuarios] AS [usuario] WITH(NOLOCK)
-      ON [pendenciahora].[IdUsuarioResp] = [usuario].[IdUsuario]
-    INNER JOIN
-  [Pendencias] AS [pendencia] WITH(NOLOCK)
-      ON [pendenciahora].[IdPendencia] = [pendencia].[IdPendencia]
-    INNER JOIN
-  Tipo_Horas AS [tipohora] WITH(NOLOCK)
-      ON [pendenciahora].[IdTipoHora] = [tipohora].[IdTipoHora]
+  INNER JOIN
+  [Usuarios] AS [usuario] WITH(NOLOCK) ON [pendenciahora].[IdUsuarioResp] = [usuario].[IdUsuario]
+  INNER JOIN
+  [Pendencias] AS [pendencia] WITH(NOLOCK) ON [pendenciahora].[IdPendencia] = [pendencia].[IdPendencia]
+  INNER JOIN
+  [Tipo_Horas] AS [tipohora] WITH(NOLOCK) ON [pendenciahora].[IdTipoHora] = [tipohora].[IdTipoHora]
+  INNER JOIN
+  [Nodes] AS [node] WITH(NOLOCK) ON [node].[IdNode] = [pendencia].[IdNode]
 WHERE
   ([pendenciahora].IdUsuarioResp = @IdUsuario)
   AND (@IdPendencia = '' OR [pendenciahora].IdPendencia = @IdPendencia)
   AND ((@Data = '' AND FORMAT(GETDATE(), 'yyyy-MM-dd') = [pendenciahora].Data) OR (@Data = [pendenciahora].Data))
-ORDER BY
-  CAST([pendenciahora].[HoraInicio] AS TIME)  
+ORDER BY CAST([pendenciahora].[HoraInicio] AS TIME)
 
 /*************************************************************************************************************************
-    
-        SELECT [IdTipoHora], [DescricaoTipoHora], [Ativo] FROM Tipo_Horas WITH(NOLOCk)
-        WHERE [IdTipoHora] IN (4,6,8,10,14,17,28,33) ORDER BY DescricaoTipoHora
+
+  [Visualizar Tipos de Hora]
+  SELECT [IdTipoHora], [DescricaoTipoHora], [Ativo] FROM Tipo_Horas WITH(NOLOCK)
+  WHERE [IdTipoHora] IN (4,6,8,10,14,17,28,33) ORDER BY DescricaoTipoHora
 
 
-                                                 [DELETAR HORA]
-                        BEGIN TRAN DELETE FROM Pendencias_Horas WHERE IdPendenciaHoras = ''
-                                
-                                              [ATUALIZAR APONTAMENTO]
-                                                  [Atualizar Hora]
-        BEGIN TRAN UPDATE Pendencias_Horas SET HoraInicio = '1900-01-01 14:17:43.000' WHERE IdPendenciaHoras = ''
-        BEGIN TRAN UPDATE Pendencias_Horas SET HoraFim = '1899-12-30 15:41:12.000' WHERE IdPendenciaHoras = ''
-                                             [Atualizar Tipo Apontamento]
-        BEGIN TRAN UPDATE Pendencias_Horas SET IdTipoHora = 6 WHERE IdPendenciaHoras = ''
+  [DELETAR HORA]
+  BEGIN TRAN DELETE FROM Pendencias_Horas WHERE IdPendenciaHoras = '<ID do Registro de Hora da Pendência, INT, >'
 
-                
-                                              [ATUALIZAR PENDENCIA]
-                                                [Trocar a Pasta]
-        BEGIN TRAN UPDATE Pendencias SET IdNode = 1656 WHERE IdPendencia = ''
+  [ATUALIZAR APONTAMENTO]
+  [Atualizar Hora]
+  BEGIN TRAN UPDATE Pendencias_Horas SET HoraInicio = '1900-01-01 14:17:43.000' WHERE IdPendenciaHoras = '<ID do Registro de Hora da Pendência, INT, >'
+  BEGIN TRAN UPDATE Pendencias_Horas SET HoraFim = '1899-12-30 15:41:12.000' WHERE IdPendenciaHoras = '<ID do Registro de Hora da Pendência, INT, >'
+  [Atualizar Tipo Apontamento]
+  BEGIN TRAN UPDATE Pendencias_Horas SET IdTipoHora = 6 WHERE IdPendenciaHoras = '<ID do Registro de Hora da Pendência, INT, >'
 
 
+  [ATUALIZAR PENDENCIA]
+  [Trocar a Pasta]
+  BEGIN TRAN UPDATE Pendencias SET IdNode = 1656 WHERE IdPendencia = '<ID da Pendência, INT, >'
 
-                                             COMMIT            ROLLBACK                                        
-                                  
-                                  
+
+                                        COMMIT            ROLLBACK
+
 *************************************************************************************************************************/
