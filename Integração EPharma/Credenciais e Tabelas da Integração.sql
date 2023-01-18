0921869239df58d451eb66ac09e09f0f4307342d
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2023-01-18 16:57:14 -0300

    Apply several changes

--- Integração EPharma/Credenciais e Tabelas da Integração.sql
+++ Integração EPharma/Credenciais e Tabelas da Integração.sql
@@ -26,10 +26,14 @@
   Senha: JgtHbfeL
   Empresa a Consultar: 11719795000370
 
-  
+
   ===================================================================================
    Comandos Auxiliares
   ===================================================================================
+  
+  -- Tabelas de configuração da Integração EPharma
+  SELECT * FROM Tab_EpharmaMoveConfig
+  SELECT * FROM Tab_ConfiguracaoEPharma
 
   Configurar a integração da EPharma para o ambiente de homologação:
   UPDATE Tab_EpharmaMoveConfig
@@ -38,27 +42,62 @@
   , [str_Usuario]    = 11719795000370
   , [str_Senha]      = 123456
 
+
+  Apagar os movimentos da base para que possam ser solicitados novamente:
+
+  -- Apaga registros do mês do ano atual, na tabela de pedido
+  DELETE [pedido] FROM Mov_EpharmaMovePedido AS [pedido]
+  WHERE (MONTH([pedido].[dte_DataResposta]) = MONTH(DATEADD(MONTH, - 3, GETDATE())) AND YEAR([pedido].[dte_DataResposta]) = YEAR(DATEADD(MONTH, - 3, GETDATE())) OR [pedido].[dte_DataResposta] IS NULL)
+
+  -- Apaga registros do mês do ano atual, na tabela de itens do pedido
+  DELETE [pedido_item] FROM Mov_EpharmaMovePedido AS [pedido] LEFT JOIN Mov_EpharmaMovePedidoItens AS [pedido_item] ON  [pedido].[pk_int_Mov_EpharmaMovePedido] = [pedido_item].[fk_int_Mov_EpharmaMovePedido]
+  WHERE (MONTH([pedido].[dte_DataResposta]) = MONTH(DATEADD(MONTH, - 3, GETDATE())) AND YEAR([pedido].[dte_DataResposta]) = YEAR(DATEADD(MONTH, - 3, GETDATE())) OR [pedido].[dte_DataResposta] IS NULL)
+
 **************************************************************************************/
 
--- Configurações da Integração EPharma
-SELECT * FROM Tab_EpharmaMoveConfig
-SELECT * FROM Tab_ConfiguracaoEPharma
-
 -- Dados do Pedido
-SELECT * FROM Mov_EpharmaMovePedido
-SELECT * FROM Mov_EpharmaMovePedidoItens
+SELECT
+  [pedido].[pk_int_Mov_EpharmaMovePedido] AS [PK do Pedido]
+, [pedido].[dte_DataResposta]             AS [Data e Hora Resposta]
+, [pedido].[dte_DataHoraPedido]           AS [Data e Hora Pedido]
+, [pedido].[str_Cnpj]                     AS [CNPJ]
+, [pedido].[int_NumeroDocumento]          AS [Número do Documento]
+, [pedido].[int_NumeroPedido]             AS [Número do Pedido]
+, [pedido_item].[str_NomeProduto]         AS [Nome do Produto]
+, [beneficiario].[pk_int_Mov_EpharmaMovePedidoDadosBeneficiario] AS [PK Beneficiario]
+, [beneficiario_endereco].[pk_int_Mov_EpharmaMovePedidoDadosBeneficiarioEndereco] AS [PK Beneficiario Endereço]
+, [beneficiario_telefone].[pk_int_Mov_EpharmaMovePedidoDadosBeneficiarioTelefones] AS [PK Beneficiario Telefone]
+, [cliente].[pk_int_Mov_EpharmaMovePedidoDadosCliente] AS [PK Cliente]
+, [clinico].[pk_int_Mov_EpharmaMovePedidoDadosClinicos] AS [PK Clinico]
+FROM
+  Mov_EpharmaMovePedido      AS [pedido]
+  FULL JOIN
+  Mov_EpharmaMovePedidoItens AS [pedido_item] ON [pedido].[pk_int_Mov_EpharmaMovePedido] = [pedido_item].[fk_int_Mov_EpharmaMovePedido]
+  FULL JOIN
+  Mov_EpharmaMovePedidoDadosBeneficiario          AS [beneficiario]          ON [beneficiario].[fk_int_Mov_EpharmaMovePedido] = [pedido].[pk_int_Mov_EpharmaMovePedido]
+  FULL JOIN
+  Mov_EpharmaMovePedidoDadosBeneficiarioEndereco  AS [beneficiario_endereco] ON [beneficiario_endereco].[fk_int_Mov_EpharmaMovePedidoDadosBeneficiario]  = [beneficiario].[pk_int_Mov_EpharmaMovePedidoDadosBeneficiario]
+  FULL JOIN
+  Mov_EpharmaMovePedidoDadosBeneficiarioTelefones AS [beneficiario_telefone] ON [beneficiario_telefone].[fk_int_Mov_EpharmaMovePedidoDadosBeneficiario] = [beneficiario].[pk_int_Mov_EpharmaMovePedidoDadosBeneficiario]
+  FULL JOIN
+  Mov_EpharmaMovePedidoDadosCliente               AS [cliente]               ON [cliente].[fk_int_Mov_EpharmaMovePedido] = [pedido].[pk_int_Mov_EpharmaMovePedido]
+  FULL JOIN
+  Mov_EpharmaMovePedidoDadosClinicos              AS [clinico]               ON [clinico].[fk_int_Mov_EpharmaMovePedido] = [pedido].[pk_int_Mov_EpharmaMovePedido]
+ORDER BY [dte_DataResposta] DESC
 
--- Dados do Beneficiário
-SELECT * FROM Mov_EpharmaMovePedidoDadosBeneficiario
-SELECT * FROM Mov_EpharmaMovePedidoDadosBeneficiarioEndereco
-SELECT * FROM Mov_EpharmaMovePedidoDadosBeneficiarioTelefones
--- Dados do Cliente e Clínico
-SELECT * FROM Mov_EpharmaMovePedidoDadosCliente
-SELECT * FROM Mov_EpharmaMovePedidoDadosClinicos
--- Dados do Convênio
-SELECT * FROM Tab_EPharma_Convenios
-SELECT * FROM Tab_EPharma_Convenios_Estado
 
--- Dados da Reposta
-SELECT * FROM Tmp_EPharmaResposta
-SELECT * FROM Tmp_EPharmaRespostaAutorizacaoItens
+/**************************************************************************************
+  
+  ===================================================================================
+   Outras Tabelas que podem ser Úteis
+  ===================================================================================
+
+  -- Dados do Convênio
+  SELECT * FROM Tab_EPharma_Convenios
+  SELECT * FROM Tab_EPharma_Convenios_Estado
+
+  -- Dados da Reposta
+  SELECT * FROM Tmp_EPharmaResposta
+  SELECT * FROM Tmp_EPharmaRespostaAutorizacaoItens
+
+**************************************************************************************/
