ee0ad76d0e40e2b8052b70fef532f426b425c63a
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2023-01-27 18:06:21 -0300

    Apply several changes

---
+++ Integration Service e Ouro Mobile/Consultas com Informações Úteis.sql
@@ -0,0 +1,32 @@
+/**************************************************************************************
+
+  Pressione "[F5]" para usar o Script abaixo.
+
+  ===================================================================================
+   Pequena Descrição do Script
+  ===================================================================================
+
+  Esse Script serve como auxílio no desenvolvimento para as pendências que utilizam
+  do "Integration Service" e "Mobile Service".
+
+**************************************************************************************/
+
+SELECT
+  CASE WHEN str_IdFuncionario IS NULL THEN CAST([Empresa].[IdEmpresa] AS VARCHAR) ELSE [Funcionario].[IdFuncionário] END AS [ID (OuroBase)]
+, CASE WHEN str_IdFuncionario IS NULL THEN [Empresa].[Empresa] ELSE [Funcionario].[NOME] END                             AS [Nome (OuroBase)]
+, [EntidadeDaIntegracao].[int_IdEmpresaIntegracao]                                                                       AS [ID (Base da Integração)]
+, CASE WHEN [EntidadeDaIntegracao].fk_int_IdEmpresaLocal <> 0 THEN [EntidadeDaIntegracao].fk_int_IdEmpresaLocal ELSE NULL END AS [LocalEstoqueID (Base da Integração)]
+, [EntidadeDaIntegracao].[str_DescricaoEmpresa]                                                                          AS [Nome (Base da Integração)]
+, CASE WHEN str_IdFuncionario IS NULL THEN 'Empresa' ELSE 'Funcionário' END                                              AS [Tipo de Registro]
+, CASE WHEN str_IdFuncionario IS NULL THEN 'Tab_Empresa' ELSE 'Tab_Funcionários' END                                     AS [Tabela]
+FROM
+  [Tab_Empresa_Integracao] AS [EntidadeDaIntegracao]
+    LEFT JOIN
+  [Tab_Empresa]            AS [Empresa]      ON [EntidadeDaIntegracao].[fk_int_IdEmpresaLocal] <> 0    AND [Empresa].[IdEmpresa]            = [EntidadeDaIntegracao].[int_IdEmpresaIntegracao]
+    LEFT JOIN
+  [Tab_LocaisEstoque]      AS [LocalEstoque] ON [EntidadeDaIntegracao].[fk_int_IdEmpresaLocal] <> 0    AND [LocalEstoque].[IdLocal]         = [EntidadeDaIntegracao].[fk_int_IdEmpresaLocal]
+    LEFT JOIN
+  [Tab_Funcionários]       AS [Funcionario]  ON [EntidadeDaIntegracao].[str_IdFuncionario] IS NOT NULL AND [Funcionario].[IdFuncionário] = [EntidadeDaIntegracao].[str_IdFuncionario]
+ORDER BY
+  [Tipo de Registro]
+, [Nome (OuroBase)]
