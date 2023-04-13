340da68c0c4fd66221bd8f16954b0cb899f482bc
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2023-04-13 17:14:53 -0300

    Apply several changes

---
+++ Templates de Doc. Técnica/Configuração.sql
@@ -0,0 +1,9 @@
+1) Rodar o aplicativo ObjetosWeb.ade no caminho padrão "C:\DvpLocal\Ferramentas";
+2) Abrir a tela de Configuração;
+3) Ir na setinha com asterisco no pé da tela, ao clicar será possivel inserir uma nova configuração;
+4) Inserir o nome da configuração no campo "Config" sem o "bln_";
+5) No campo "Tipo Config" escolher "BOOLEAN";
+6) No campo Descrição inserir: <Número da Pendência, , 69345> - <Descrição da Configuração, , Habilita Detalhes Item>;
+7) Os módulos a serem adicionadas a configuração favor verificar na descrição da pendência ou com o solicitante;
+8) Gerar e rodar o script de insert, localizado por padrão em "C:\DvpLocal\ScriptConfigCampo";
+9) O script deve ser adicionado ao pacote de scripts OuroNet.
